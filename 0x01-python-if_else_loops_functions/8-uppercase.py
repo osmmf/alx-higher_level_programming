a4c1e388412f6f13c6db9977f97ac7c16cf08424
”osmmf” <elmeftahioussama@gmail.com>
2023-09-06 13:11:49 -0700

    1

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,7 @@ def islower(c):
         return True
     else:
         return False
-
 def uppercase(str):
     for c in str:
-        print("{:c}".format(ord(c) if not islower(c) else ord(c) - 32), end="\n")
+        print("{:c}".format(ord(c) if not islower(c) else ord(c) - 32), end="")
     print("")

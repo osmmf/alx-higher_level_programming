9b38d9b202e2e88a29c961090da970cef0a6f9df
”osmmf” <elmeftahioussama@gmail.com>
2023-09-07 15:18:42 -0700

    “commit”

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys 
     sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print("{}".format(sum))
+    for x in range(len(sys.argv) - 1):
+        sum += int(sys.argv[x + 1])
+    print(sum)

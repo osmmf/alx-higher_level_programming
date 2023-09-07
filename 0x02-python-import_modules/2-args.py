010ea09f1ed7266f2bb8f6a69022a6edf68c5495
”osmmf” <elmeftahioussama@gmail.com>
2023-09-07 15:07:04 -0700

    “commit”

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys 
 
-    cnt = len(sys.argv) - 1
-    if cnt == 0:
+if __name__ == "__main__":
+    """Print the number and list of arguments"""
+    import sys
+
+    if len(sys.argv) == 1:
         print("0 arguments.")
-    elif cnt == 1:
-        print("1 arguments:")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(cnt))
-    for x in range(cnt):
-        print("{}: {}".format(x +1, sys.argv[x + 1]))
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv) - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))

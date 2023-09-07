022945d91508cdb7349774b896c79a9ca464be49
”osmmf” <elmeftahioussama@gmail.com>
2023-09-07 15:03:17 -0700

    “commit”

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys 
+
+    cnt = len(sys.argv) - 1
+    if cnt == 0:
+        print("0 arguments.")
+    elif cnt == 1:
+        print("1 arguments:")
+    else:
+        print("{} arguments".format(cnt))
+    for x in range(cnt):
+        print("{}: {}".format(x +1, sys.argv[x + 1]))

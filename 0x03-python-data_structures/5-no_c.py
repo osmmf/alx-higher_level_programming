b1642dc224f9c301f85119aaaf4c8bc80fabc283
”osmmf” <elmeftahioussama@gmail.com>
2023-09-11 15:13:02 -0700

    “commit”

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    rtrn = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            rtrn += my_string[i]
-    return rtrn
+    str = ""
+    for i in my_string:
+        if i not in 'Cc':
+            str += i
+    return str

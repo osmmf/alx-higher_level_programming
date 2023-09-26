4e215cfcb1e069896933a4ccba6968c925e9ab16
”osmmf” <elmeftahioussama@gmail.com>
2023-09-26 12:19:43 -0700

    Updated

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square():
+    """ the class square """
+    def __init__(self, __size=0) -> None:
+        if type(__size) != int:
+            raise TypeError("size must be an integer")
+        elif __size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = __size
